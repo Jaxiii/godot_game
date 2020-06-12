3961b9b1d1f31d5d8e65d4eb5e2a50af617ba20e
Bruno <brunoebarros@gmail.com>
2020-06-12 15:12:44 -0300

    init commit

---
+++ Player/Player.gd
@@ -0,0 +1,28 @@
+extends KinematicBody2D
+
+const ACCELERATION = 550
+const MAX_SPEED = 150
+const FRICTION = 840
+
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	
+	var input_vector = Vector2.ZERO
+	
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		if (Input.get_action_strength("ui_home")):
+			velocity = velocity.move_toward(input_vector * (MAX_SPEED + 500), ACCELERATION * delta)
+		elif (Input.get_action_strength("ui_end")):
+			velocity = velocity.move_toward(input_vector * (MAX_SPEED - 100), ACCELERATION * delta)
+		else:
+			velocity = velocity.move_toward(input_vector * (MAX_SPEED), ACCELERATION * delta)
+			
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+	velocity = move_and_slide(velocity)
